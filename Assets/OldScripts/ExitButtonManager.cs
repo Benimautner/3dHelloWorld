c5f6ee7e4b3fe39013fd62ee55ba573f1fddbd0c
benimautner <benimautner@gmail.com>
2020-12-27 17:20:50 +0100

    See previous commit

---
+++ Assets/OldScripts/ExitButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Button btn;
+    
+    void Start()
+    {
+        btn.onClick.AddListener(onClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        }
+
+    void onClick()
+    {
+        if (Application.isEditor)
+            EditorApplication.isPlaying = false;
+        else
+            Application.Quit();    
+    }
+
+}
---
+++ Assets/OldScripts/PlaneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private Plane _plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
