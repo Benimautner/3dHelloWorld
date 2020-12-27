156f3d4579bf064846e2dfc69853c40dd354135a
benimautner <benimautner@gmail.com>
2020-12-27 17:22:34 +0100

    Moved handling of pause menu buttons into PauseMenuController

--- Assets/PauseMenuController.cs
+++ Assets/PauseMenuController.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using DefaultNamespace;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PauseMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button resumeButton;
+    
     // Start is called before the first frame update
     void Start()
     {
+        exitButton.onClick.AddListener(ExitGame);
+        resumeButton.onClick.AddListener(HideMenu);
+        mainMenuButton.onClick.AddListener(BackToMenu);
         pauseMenu.SetActive(false);
     }
 
@@ -45,5 +57,18 @@ public class PauseMenuController : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         SharedInfo.InMenu = true;
+    }
+    
+    void ExitGame()
+    {
+        if (Application.isEditor)
+            EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+
+    void BackToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 }
