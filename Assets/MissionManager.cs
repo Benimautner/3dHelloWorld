218c1bac62e7adf085bf279297f5be197cfb8497
benimautner <benimautner@gmail.com>
2021-01-08 00:52:56 +0100

    added serializing and deserializing of json to string

--- Assets/MissionManager.cs
+++ Assets/MissionManager.cs
@@ -19,20 +19,30 @@ public class MissionManager : MonoBehaviour
     public GameObject arrow;
     public GameObject player;
     public GameObject head;
-    
-    private Vector3 _target;
+    private Vector2 _target;
+    private int _currentMission;
+    public GameObject beacon;
+
+    public List<Vector2> missions;
 
     private void Start()
     {
-        _target = new Vector3(10, 10, 10);
-        
-        LoadJson();
-
+        missions = LoadJson();
+        _target = missions[0];
+        beacon.transform.position = new Vector3(_target.x, 0, _target.y);
     }
 
     private void Update()
     {
-        Vector3 dir = (_target) - (player.transform.position);
+        Vector3 playerPos = player.transform.position;
+        if (Vector2.Distance(_target, new Vector2(playerPos.x, playerPos.z)) < 5.0f) {
+            _currentMission++;
+            if (_currentMission > missions.Count - 1)
+                _currentMission = 0;
+            _target = missions[_currentMission];
+            beacon.transform.position = new Vector3(_target.x, 0, _target.y);
+        }
+        Vector3 dir = (new Vector3(_target.x, 0, _target.y)) - (playerPos);
 
         float x = Vector2.SignedAngle(Vector3ToVector2Horizontal(dir), Vector3ToVector2Horizontal(head.transform.forward));
         
@@ -44,8 +54,9 @@ public class MissionManager : MonoBehaviour
         return new Vector2(vin.x, vin.z);
     }
 
-    public void LoadJson()
+    public List<Vector2> LoadJson()
     {
+        List<Vector2> returnVecs = new List<Vector2>();
         
         print(Application.streamingAssetsPath);
 
@@ -60,22 +71,26 @@ public class MissionManager : MonoBehaviour
             //FileStream fs = File.Create(Application.streamingAssetsPath + "/missions.save");
             GameInfo gameInfo = new GameInfo();
             gameInfo.missions = new Dictionary<string, Tuple<float, float>>();
-            gameInfo.missions.Add("first", new Tuple<float, float>(564, 5));
-            gameInfo.missions.Add("second", new Tuple<float, float>(88652, 5563));
-            gameInfo.missions.Add("third", new Tuple<float, float>(1, 1));
-            gameInfo.missions.Add("fourth", new Tuple<float, float>(56774, 860));
+            gameInfo.missions.Add("first", new Tuple<float, float>(50, 50));
+            gameInfo.missions.Add("second", new Tuple<float, float>(70, 50));
+            gameInfo.missions.Add("third", new Tuple<float, float>(0, 0));
+            gameInfo.missions.Add("fourth", new Tuple<float, float>(-30, -40));
 
 
-            
             string json = Json.Serialize(gameInfo.missions);
-            print("json: " + json);
+            //print("json: " + json);
+
             var newGameInfo = Json.Deserialize(json) as Dictionary<string, object>;
+            if (newGameInfo == null) return new List<Vector2>();
             foreach (var obj in newGameInfo) {
-                print("key: " + obj.Key + " Tuple: " + ParseStringToTuple(obj.Value.ToString()));
+                Tuple<float, float> tuple = ParseStringToTuple(obj.Value.ToString());
+                Vector2 newVec = new Vector2(tuple.Item1, tuple.Item2);
+                returnVecs.Add(newVec);
             }
 
         }
 
+        return returnVecs;
     }
 
     private Tuple<float, float> ParseStringToTuple(string inString)
@@ -87,8 +102,6 @@ public class MissionManager : MonoBehaviour
         
         string val1 = inString.Substring(start+1,endFirst-start-1);
         string val2 = inString.Substring(endFirst+1, endLast-endFirst-1);
-        
-        print(val1);
         
         return new Tuple<float, float>(int.Parse(val1), int.Parse(val2));
     }
