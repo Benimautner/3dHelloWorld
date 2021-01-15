20ed7fc0bffcd2a086cc4216636e0191111cb3b4
benimautner <benimautner@gmail.com>
2021-01-15 23:20:02 +0100

    added initialized bool

--- Assets/SharedInfo.cs
+++ Assets/SharedInfo.cs
@@ -5,6 +5,7 @@ namespace DefaultNamespace
 {
     public static class SharedInfo
     {
+        public static bool initialized = false;
         public static bool InMenu;
         public static Vector3[] MapVertices;
         public static AnimationCurve terrainHeightCurve;
